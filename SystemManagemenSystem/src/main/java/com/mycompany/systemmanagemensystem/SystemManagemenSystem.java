1bc9c32428b5392ba627e35c5258996e96c4f3e2
admin <admin@DESKTOP-MIGFL05>
2024-04-07 23:15:51 +0800

    System Management

---
+++ SystemManagemenSystem/src/main/java/com/mycompany/systemmanagemensystem/SystemManagemenSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.systemmanagemensystem;
+
+/**
+ *
+ * @author admin
+ */
+public class SystemManagemenSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
